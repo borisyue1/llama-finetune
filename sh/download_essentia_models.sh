eca04f18847d83213e93c42b9e6d493e39f4a1e3
Boris Yue <byue@thayermahan.com>
2023-12-29 10:25:55 -0800

    Create essentia labels file

---
+++ sh/download_essentia_models.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Needed for the tensorflow modules to install properly!
+sudo apt-get install build-essential libeigen3-dev libyaml-dev libfftw3-dev libtag1-dev libchromaprint-dev
+
+# Download essentia model weights for metadata labels
+model_path=models/essentia
+mkdir -p $model_path
+curl https://essentia.upf.edu/models/classification-heads/genre_discogs400/genre_discogs400-discogs-effnet-1.pb --output $model_path/genre_discogs400-discogs-effnet-1.pb
+curl https://essentia.upf.edu/models/feature-extractors/discogs-effnet/discogs-effnet-bs64-1.pb --output $model_path/discogs-effnet-bs64-1.pb
+curl https://essentia.upf.edu/models/classification-heads/mtg_jamendo_moodtheme/mtg_jamendo_moodtheme-discogs-effnet-1.pb --output $model_path/mtg_jamendo_moodtheme-discogs-effnet-1.pb
+curl https://essentia.upf.edu/models/classification-heads/mtg_jamendo_instrument/mtg_jamendo_instrument-discogs-effnet-1.pb --output $model_path/mtg_jamendo_instrument-discogs-effnet-1.pb
