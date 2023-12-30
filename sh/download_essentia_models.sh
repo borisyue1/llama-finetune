f4719a646196b8f3cd4a9c4a3d328e22c381305c
Boris Yue <byue@thayermahan.com>
2023-12-29 19:52:14 -0800

    Save notebook progress with training fixed

--- sh/download_essentia_models.sh
+++ sh/download_essentia_models.sh
@@ -6,7 +6,11 @@ sudo apt-get install build-essential libeigen3-dev libyaml-dev libfftw3-dev libt
 # Download essentia model weights for metadata labels
 model_path=models/essentia
 mkdir -p $model_path
-curl https://essentia.upf.edu/models/classification-heads/genre_discogs400/genre_discogs400-discogs-effnet-1.pb --output $model_path/genre_discogs400-discogs-effnet-1.pb
+# Feature embedding model
 curl https://essentia.upf.edu/models/feature-extractors/discogs-effnet/discogs-effnet-bs64-1.pb --output $model_path/discogs-effnet-bs64-1.pb
+# Genre classification
+curl https://essentia.upf.edu/models/classification-heads/genre_discogs400/genre_discogs400-discogs-effnet-1.pb --output $model_path/genre_discogs400-discogs-effnet-1.pb
+# Mood classification
 curl https://essentia.upf.edu/models/classification-heads/mtg_jamendo_moodtheme/mtg_jamendo_moodtheme-discogs-effnet-1.pb --output $model_path/mtg_jamendo_moodtheme-discogs-effnet-1.pb
+# Instrument classification
 curl https://essentia.upf.edu/models/classification-heads/mtg_jamendo_instrument/mtg_jamendo_instrument-discogs-effnet-1.pb --output $model_path/mtg_jamendo_instrument-discogs-effnet-1.pb
